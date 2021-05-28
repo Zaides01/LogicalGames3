f878e44a21f7f38b552395b0ee68d1e3fc852d82
Zaides01 <zajdesnuska@gmail.com>
2021-05-29 01:08:53 +0800

    release

---
+++ app/src/main/java/com/example/logicalgames/User.java
@@ -0,0 +1,11 @@
+package com.example.logicalgames;
+
+public class User {
+    String uid;
+    int rating;
+
+    public User(String uid, int rating) {
+        this.uid = uid;
+        this.rating = rating;
+    }
+}
